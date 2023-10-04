c03e0f5095989c50f1ac4d6b756e59d59fd44e0e
zhangrongluo <12281842+zhangrongluo@user.noreply.gitee.com>
2023-10-05 00:07:51 +0800

    完善了更新和测试时间互斥的问题

--- task.py
+++ task.py
@@ -17,42 +17,49 @@ import swindustry as sw
 import data
 import test
 from path import TEST_CONDITION_SQLITE3
+import threading
 
+lock = threading.Lock()
 scheduler = BackgroundScheduler()
 codes = [item[0][0:6] for item in sw.get_all_stocks()]
 
 # 每日下午6点05分开始更新一次trade record csv文件
-@scheduler.scheduled_job('cron', hour=19, minute=5, id='update_trade_record_csv')
 def update_trade_record_csv():
-    print('开始更新trade record csv文件\r', end='', flush=True)
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(data.update_trade_record_csv, codes)
-    print('更新trade record csv文件完成' + ' '*20, flush=True)
+    with lock:
+        print('开始更新trade record csv文件\r', end='', flush=True)
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            executor.map(data.update_trade_record_csv, codes)
+        print('更新trade record csv文件完成' + ' '*20, flush=True)
 
 # 每日下午6点10分开始更新一次curve.sqlite3
-@scheduler.scheduled_job('cron', hour=19, minute=10, id='update_curve_sqlite3')
 def update_curve_sqlite3():
-    print('开始更新curve.sqlite3\r', end='', flush=True)
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(data.update_curve_sqlite3, codes)
-    print('更新curve.sqlite3完成' + ' '*20, flush=True)
+    with lock:
+        print('开始更新curve.sqlite3\r', end='', flush=True)
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            executor.map(data.update_curve_sqlite3, codes)
+        print('更新curve.sqlite3完成' + ' '*20, flush=True)
 
 # 每周五上午7点30分将TEST_CONDITION_SQLITE3拷贝到本地仓库,更名为test-condition-quant.sqlite3
 # 本地仓库路径: /Users/zhangrongluo/Desktop/pythonzone/win-stock-conditions/win-stock-conditions
 # 每周五上午8点推送到github
-@scheduler.scheduled_job('cron', day_of_week='fri', hour=7, minute=30, id='copy_test_condition_sqlite3')
 def copy_test_condition_sqlite3():
-    import platform
-    if platform.machine() == "x86_64":  # (在imac机器上执行)
-        print('开始拷贝TEST_CONDITION_SQLITE3\r', end='', flush=True)
-        des_path = "/Users/zhangrongluo/Desktop/pythonzone/win-stock-conditions/win-stock-conditions"  # 本地仓库路径
-        shutil.copyfile(TEST_CONDITION_SQLITE3, os.path.join(des_path, "test-condition-quant.sqlite3"))
-        print('拷贝TEST_CONDITION_SQLITE3完成' + ' '*20, flush=True)
+    with lock:
+        import platform
+        if platform.machine() == "x86_64":  # (在imac机器上执行)
+            print('开始拷贝TEST_CONDITION_SQLITE3\r', end='', flush=True)
+            des_path = "/Users/zhangrongluo/Desktop/pythonzone/win-stock-conditions/win-stock-conditions"  # 本地仓库路径
+            shutil.copyfile(TEST_CONDITION_SQLITE3, os.path.join(des_path, "test-condition-quant.sqlite3"))
+            print('拷贝TEST_CONDITION_SQLITE3完成' + ' '*20, flush=True)
 
 def run():
+    scheduler.add_job(update_trade_record_csv, 'cron', hour=23, minute=50)
+    scheduler.add_job(update_curve_sqlite3, 'cron', hour=18, minute=10)
+    scheduler.add_job(copy_test_condition_sqlite3, 'cron', day_of_week=5, hour=7, minute=30)
     scheduler.start()
-    scheduler.print_jobs()
-    test.auto_test()  # 每天下午6点至7点30分之外的时间执行自动测试任务
+    thread = threading.Thread(target=test.auto_test)
+    thread.start()
+    while True:
+        time.sleep(1)
 
 if __name__ == '__main__':
     run()
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 from strategy import Strategy
 from path import TEST_CONDITION_SQLITE3
+import threading
 
 # quant-stock系统所有测试条件保存在test-condition.sqlite3数据库中,每个年份一个表,用于保存该年度的全部测试条件,表名格式为condition-年份.
 # 每年的1-5月生成的测试条件保存在上年的表中,比如2023年1-5月生成的测试条件保存在condition-2022表中.
@@ -46,67 +47,63 @@ def auto_test():
     每天下午6点至7点30分之外的时间执行自动测试任务
     """
     case = Strategy()
+    lock = threading.Lock()
     while True:
-        now = time.localtime()
-        table_name = f'condition-{now.tm_year}' if now.tm_mon >= 5 else f'condition-{now.tm_year-1}'
-        if now.tm_mon in [1, 2, 3, 4, 5]:
-            # 设置当年的flag值为No
-            con = sqlite3.connect(TEST_CONDITION_SQLITE3)
-            with con:
-                sql = f"""
-                    INSERT OR IGNORE INTO flag (year, flag) VALUES ('{time.localtime().tm_year}', 'No')
-                """
-                con.execute(sql)
-        elif now.tm_mon in [6, 7, 8, 9, 10, 11, 12]:
-            # 创建年度表格
-            con = sqlite3.connect(TEST_CONDITION_SQLITE3)
-            with con:
-                sql = f"""
-                    CREATE TABLE IF NOT EXISTS '{table_name}'
-                    (
-                        strategy TEXT, 
-                        test_condition TEXT, 
-                        total_groups INTEGER, 
-                        valid_groups INTEGER, 
-                        valid_percent REAL, 
-                        valid_groups_keys TEXT, 
-                        basic_ratio REAL, 
-                        inner_rate REAL, 
-                        score REAL, 
-                        date TEXT,
-                        PRIMARY KEY(strategy, test_condition)
-                    )
-                """
-                con.execute(sql)
-
-                # 从以前年度表格中获取测试条件集合,执行retest_conditions_from_sqlite3函数,并保存结果到condition-2023表中.
-                if has_test_previous_year() is False:
-                    # 获取以前年度表格的表名(不包含当年的表)
-                    df = pd.read_sql('SELECT name FROM sqlite_master WHERE type="table"', con)
-                    all_table_names = df['name'].tolist()
-                    prev_table_names = [table for table in all_table_names if str(now.tm_year) not in table and table != 'flag']
-
-                    for prev_table in prev_table_names:
-                        case.retest_conditions_from_sqlite3(src_sqlite3=TEST_CONDITION_SQLITE3, src_table=prev_table, dest_sqlite3=TEST_CONDITION_SQLITE3, dest_table=table_name)
-
-                    # 如果没有当年的flag记录,则插入当年的flag值为Yes
+        with lock:
+            now = time.localtime()
+            table_name = f'condition-{now.tm_year}' if now.tm_mon >= 5 else f'condition-{now.tm_year-1}'
+            if now.tm_mon in [1, 2, 3, 4, 5]:
+                # 设置当年的flag值为No
+                con = sqlite3.connect(TEST_CONDITION_SQLITE3)
+                with con:
                     sql = f"""
-                        INSERT OR IGNORE INTO flag (year, flag) VALUES ('{time.localtime().tm_year}', 'Yes')
+                        INSERT OR IGNORE INTO flag (year, flag) VALUES ('{time.localtime().tm_year}', 'No')
+                    """
+                    con.execute(sql)
+            elif now.tm_mon in [6, 7, 8, 9, 10, 11, 12]:
+                # 创建年度表格
+                con = sqlite3.connect(TEST_CONDITION_SQLITE3)
+                with con:
+                    sql = f"""
+                        CREATE TABLE IF NOT EXISTS '{table_name}'
+                        (
+                            strategy TEXT, 
+                            test_condition TEXT, 
+                            total_groups INTEGER, 
+                            valid_groups INTEGER, 
+                            valid_percent REAL, 
+                            valid_groups_keys TEXT, 
+                            basic_ratio REAL, 
+                            inner_rate REAL, 
+                            score REAL, 
+                            date TEXT,
+                            PRIMARY KEY(strategy, test_condition)
+                        )
                     """
                     con.execute(sql)
 
-                    # 更新当年的flag值为Yes
-                    sql = f"""
-                        UPDATE flag SET flag='Yes' WHERE year='{time.localtime().tm_year}'
-                    """
-                    con.execute(sql)
-        
-        # 每天下午6点至8点之间更新数据,停止测试.
-        if now.tm_hour in [18, 19]:
-            print(f"当前时间{now.tm_hour:2d}:{now.tm_min:2d}:{now.tm_sec:2d},系统更新数据.\r" + ' '*20, end='', flush=True)
-            time.sleep(1)
-        else:
-            case.auto_test()
+                    # 从以前年度表格中获取测试条件集合,执行retest_conditions_from_sqlite3函数,并保存结果到condition-2023表中.
+                    if has_test_previous_year() is False:
+                        # 获取以前年度表格的表名(不包含当年的表)
+                        df = pd.read_sql('SELECT name FROM sqlite_master WHERE type="table"', con)
+                        all_table_names = df['name'].tolist()
+                        prev_table_names = [table for table in all_table_names if str(now.tm_year) not in table and table != 'flag']
+
+                        for prev_table in prev_table_names:
+                            case.retest_conditions_from_sqlite3(src_sqlite3=TEST_CONDITION_SQLITE3, src_table=prev_table, dest_sqlite3=TEST_CONDITION_SQLITE3, dest_table=table_name)
+
+                        # 如果没有当年的flag记录,则插入当年的flag值为Yes
+                        sql = f"""
+                            INSERT OR IGNORE INTO flag (year, flag) VALUES ('{time.localtime().tm_year}', 'Yes')
+                        """
+                        con.execute(sql)
+
+                        # 更新当年的flag值为Yes
+                        sql = f"""
+                            UPDATE flag SET flag='Yes' WHERE year='{time.localtime().tm_year}'
+                        """
+                        con.execute(sql)
+            case.test_strategy_random_condition()
         
 if __name__ == '__main__':
     auto_test()
